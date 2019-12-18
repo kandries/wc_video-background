700494f3cd21496f6f6636e9c4d701ed623ba73d
Kevin Andries Back <keadi@vestas.com>
2019-12-18 13:31:20 +0100

    init Stencil

---
+++ stencil.config.ts
@@ -0,0 +1,18 @@
+import { Config } from '@stencil/core';
+
+export const config: Config = {
+  namespace: 'videobackgroundscroll',
+  outputTargets: [
+    {
+      type: 'dist',
+      esmLoaderPath: '../loader'
+    },
+    {
+      type: 'docs-readme'
+    },
+    {
+      type: 'www',
+      serviceWorker: null // disable service workers
+    }
+  ]
+};
